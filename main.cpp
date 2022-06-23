378f9d852078ee49b07f0578f41fa6f27482d969
Sasisekhar_LAB <sasisekhar4817@gmail.com>
2022-06-23 19:11:52 -0400

    B-L4S5I-IOT01A

--- main.cpp
+++ main.cpp
@@ -2,34 +2,22 @@
 #include "ThisThread.h"
 // main() runs in its own thread in the OS
 
-DigitalOut led1(D12);
-DigitalOut led2(D13);
+DigitalOut led1(D13);
 DigitalIn button(BUTTON1);
 
 int main() {
     led1 = false;
-    led2 = false;
 
     MQTTDriver client;
     client.init();
-
-    client.connect("ARSLAB_4S5IIOT01_1");
-
-    client.subscribe("ARSLAB/Control/AC");
-
-    client.subscribe("ARSLAB/Control/Door");
-
-    client.subscribe("ARSLAB/Control/Light");
-
+    client.connect("ARSLAB_4S5IIOT01");
     client.subscribe("ARSLAB/ping/resp");
 
     for(int i = 0; i < 4; i++) {
         led1 = true;
-        led2 = true;
         ThisThread::sleep_for(100ms);
 
         led1 = false;
-        led2 = false;
         ThisThread::sleep_for(100ms);
     }
 
@@ -41,46 +29,23 @@ int main() {
     while (true) {
 
         if((us_ticker_read()/1000) -  startTime > 30000) {
-            int temp = rand()%50;
-            int hum = rand()%100;
-            int co2 = rand()%5000;
-
-            char buff[128];
-
-            sprintf(buff, "{\"Temp\":%d, \"Hum\":%d, \"CO2\":%d}", temp, hum, co2);
-
-            client.publish("ARSLAB/Data/Raw", buff);
+            client.ping();
             startTime = us_ticker_read()/1000;
         }
 
-        if(client.receive_response(topic, message)) {
-            if(!strcmp(topic, (char*) "ARSLAB/Control/Door")) {
-                if(!strcmp(message, (char*) "1")) {
-                    led1 = true;
-                } else {
-                    led1 = false;
-                }
-            } else if (!strcmp(topic, (char*) "ARSLAB/Control/Light")) {
-                if(!strcmp(message, (char*) "1")) {
-                    led2 = true;
-                } else {
-                    led2 = false;
-                }
-            } else if(!strcmp(topic, (char*) "ARSLAB/ping/resp")) {
-                if(!strcmp(message, (char*) "1")) {
-                    led1 = true;
-                    led2 = true;
-                } else if (!strcmp(message, (char*) "0")){
-                    led1 = false;
-                    led2 = false;
-                }
-            } 
-        }
+        // if(client.receive_response(topic, message)) {
+        //     if(!strcmp(topic, (char*) "ARSLAB/ping/resp")) {
+        //         if(!strcmp(message, (char*) "1")) {
+        //             led1 = true;
+        //         } else if (!strcmp(message, (char*) "0")){
+        //             led1 = false;
+        //         }
+        //     } 
+        // }
 
         if(!button) {
             if(!currState) {
                 client.publish("ARSLAB/ping/req",(char*) "1");
-
                 currState = true;
             }
 
@@ -89,10 +54,8 @@ int main() {
                     if(!strcmp(topic, (char*) "ARSLAB/ping/resp")) {
                         if(!strcmp(message, (char*) "1")) {
                             led1 = true;
-                            led2 = true;
                         } else if (!strcmp(message, (char*) "0")){
                             led1 = false;
-                            led2 = false;
                         }
                     }
 

a67f6e2fa8873ed1a2d1b75c3ccc976d519ad232
Sx3K <sasisekhar4817@gmail.com>
2022-06-30 14:06:44 -0400

    DAQ_2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "MQTTDriver.h"
-#include <string>
 // main() runs in its own thread in the OS
 
 DigitalOut led1(D13);
@@ -31,22 +30,26 @@ int main() {
 
     uint64_t startTime = 0;
     char topic[128];
-    string message;
+    char message[128];
 
     while (true) {
 
-        if((us_ticker_read()/1000) -  startTime > 30000) {
-            //client.ping();
+        if((us_ticker_read()/1000) -  startTime > 5000) {
+            char buff[128];
+
+            sprintf(buff, "{\"Temp\": {\"Temp1\": %d, \"Temp2\": %d, \"Temp3\": %d}, \"Hum\": {\"Hum1\": %d, \"Hum2\": %d, \"Hum3\": %d}, \"CO\": {\"CO1\": %d, \"CO2\": %d, \"CO3\": %d}}", rand()%50, rand()%50, rand()%50, rand()%100, rand()%100, rand()%100, rand()%5000, rand()%5000,rand()%5000);
+
+            client.publish("ARSLAB/Data/Raw", buff);
             startTime = us_ticker_read()/1000;
         }
 
-        if(client.receive_response(topic,(char*) message.c_str())) {
-            if(!strcmp(topic, (char*) "ARSLAB/Contorl/Light")) {
-                led1 = ~led1;
+        if(client.receive_response(topic, message)) {
+            if(!strcmp(topic, (char*) "ARSLAB/Control/Light")) {
+                led1 = !led1;
             }
         }
 
-        if(button) {
+        if(!button) {
             break;
         }
     }
